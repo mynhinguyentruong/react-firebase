0cb300bac53f214a0ed1e3aa942a66e1f462a9cc
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-24 10:33:06 -0400

    Create schema so we don't have to check firestore everytime

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is scream body',
+      createdAt: '2022-08-24T14:29:43.948Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
