c2ec300f40b68946e828421de46f62fe6187ccce
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-24 08:53:02 -0400

    create new scream and add to database when user send request

--- functions/index.js
+++ functions/index.js
@@ -20,3 +20,19 @@ exports.getScreams = functions.https.onRequest((req, res) => {
   })
   .catch(err => console.log(err)) 
 })
+
+exports.createScreams = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle, 
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  }
+
+  admin.firestore().collection('screams').add(newScream).then(doc => {
+    res.json({message: `document ${doc.id} created successfully`})
+    .catch(err => {
+      res.status(500).json({error: 'something went wrong'})
+      console.error(err)
+    })
+  })
+})
